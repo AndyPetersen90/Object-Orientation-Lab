36e6e7c4da97230b08bf94f34823856ba862ca26
Andrew Petersen <fespetersen1990@gmail.com>
2022-04-19 14:08:49 -0600

    Problem 3 solved

--- lab.js
+++ lab.js
@@ -30,14 +30,13 @@ let objectDog = {
 
 let dog = objectDog
 
-console.log(dog);
 
 //////////////////////////// PROBLEM 3 ////////////////////////////
 /* Print out the name of the dog you created in problem 2 using dot-notation. */
 
 //Code here
 
-
+console.log(objectDog.name);
 
 //////////////////////////// PROBLEM 4 ////////////////////////////
 /* Print out the color of the dog you created in problem 2 using bracket-notation. */
